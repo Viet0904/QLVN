72dd68c07af806494a81affdfaf5d1317969e93f
Viet0904 <viet0944840971_viet0904@gmail.com>
2025-12-21 12:18:34 +0700

    them cac file css,js. dang loi settingtheme

---
+++ QLVN-Blazor/wwwroot/js/data-table-userlist.js
@@ -0,0 +1,32 @@
+﻿var userDataTable;
+
+window.initUserDataTable = function () {
+    if ($.fn.dataTable.isDataTable('#user-datatable')) {
+        userDataTable = $('#user-datatable').DataTable();
+        return;
+    }
+
+    userDataTable = $('#user-datatable').DataTable({
+        responsive: true,
+        pageLength: 10,
+        autoWidth: false,
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/vi.json'
+        },
+        columnDefs: [
+            {
+                targets: -1,
+                orderable: false
+            }
+        ]
+    });
+};
+
+window.reinitUserDataTable = function () {
+    if (userDataTable) {
+        userDataTable.destroy();
+    }
+    setTimeout(function () {
+        window.initUserDataTable();
+    }, 200);
+};
