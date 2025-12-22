b21c691331a3f3fa819a5d8fbe43fe23f0d41f53
Nguyen Quoc Viet <nguyenquocviet@idiseafood.com>
2025-12-22 11:41:57 +0700

    update settingtheme

--- QLVN-Blazor/wwwroot/js/script.js
+++ QLVN-Blazor/wwwroot/js/script.js
@@ -1,151 +1,157 @@
 ﻿"use strict";
-
-
-console.log("QLVN: script.js is loading...");
-
-"use strict";
-
-window.initThemeSettings = function () {
-    // --- 1. Xử lý Menu Type (Simple/Color Icon) ---
-    window.handleMenuType = function (value) {
-        $('.pcoded-navbar').attr('pcoded-menutype', value);
-    };
-
-    // --- 2. Xử lý Colors (Header, Logo, Active Link, Caption) ---
-    $('.navbar-theme').off('click').on('click', function () {
-        var ntheme = $(this).attr('navbar-theme');
-        $('.pcoded').attr('navbar-theme', ntheme);
-    });
-
-    $('.logo-theme').off('click').on('click', function () {
-        var ltheme = $(this).attr('logo-theme');
-        $('.pcoded').attr('logo-theme', ltheme);
-    });
-
-    $('.header-theme').off('click').on('click', function () {
-        var htheme = $(this).attr('header-theme');
-        $('.pcoded').attr('header-theme', htheme);
-    });
-
-    $('.active-item-theme').off('click').on('click', function () {
-        var atheme = $(this).attr('active-item-theme');
-        $('.pcoded').attr('active-item-theme', atheme);
-    });
-
-    $('.leftheader-theme').off('click').on('click', function () {
-        var ltheme = $(this).attr('lheader-theme');
-        $('.pcoded').attr('lheader-theme', ltheme);
-    });
-
-    // --- 3. Xử lý Dropdown Icons & Effects ---
-    $('#vertical-menu-effect').off('change').on('change', function () {
-        $('.pcoded').attr('vertical-menu-effect', $(this).val());
-    });
-
-    $('#vertical-border-style').off('change').on('change', function () {
-        $('.pcoded').attr('vertical-border-style', $(this).val());
-    });
-
-    $('#vertical-dropdown-icon').off('change').on('change', function () {
-        $('.pcoded').attr('vertical-dropdown-icon', $(this).val());
-    });
-
-    $('#vertical-subitem-icon').off('change').on('change', function () {
-        $('.pcoded').attr('vertical-subitem-icon', $(this).val());
-    });
-
-    // --- 4. Xử lý Sidebar/Header Position ---
-    $('#sidebar-position').off('change').on('change', function () {
-        if ($(this).is(":checked")) {
-            $('.pcoded').attr('pcoded-navbar-position', 'fixed');
-            $('.pcoded-navbar').attr('pcoded-header-position', 'fixed');
-        } else {
-            $('.pcoded').attr('pcoded-navbar-position', 'relative');
-            $('.pcoded-navbar').attr('pcoded-header-position', 'relative');
-        }
-    });
-
-    $('#header-position').off('change').on('change', function () {
-        if ($(this).is(":checked")) {
-            $('.pcoded').attr('pcoded-header-position', 'fixed');
-            $('.pcoded-main-container').css('margin-top', $(".pcoded-header").outerHeight());
-        } else {
-            $('.pcoded').attr('pcoded-header-position', 'relative');
-            $('.pcoded-main-container').css('margin-top', '0px');
-        }
-    });
-};
-
-// Hàm khởi tạo các hiệu ứng UI, gọi sau khi Blazor đã render xong Component
-window.initAdminFunctions = function () {
+$(document).ready(function () {
     var $window = $(window);
-
-    // Gán ID cho main menu mobile
+    //add id to main menu for mobile menu start
     var getBody = $("body");
     var bodyClass = getBody[0].className;
     $(".main-menu").attr('id', bodyClass);
+    //add id to main menu for mobile menu end
 
-    // Card JS: Close, Minimize, Full-card
-    // Sử dụng .off().on() để tránh việc gắn sự kiện nhiều lần khi render lại
-    $(".card-header-right .close-card").off('click').on('click', function () {
+    // card js start
+    $(".card-header-right .close-card").on('click', function () {
         var $this = $(this);
         $this.parents('.card').animate({
             'opacity': '0',
             '-webkit-transform': 'scale3d(.3, .3, .3)',
             'transform': 'scale3d(.3, .3, .3)'
         });
+
         setTimeout(function () {
             $this.parents('.card').remove();
         }, 800);
     });
 
-    $(".card-header-right .minimize-card").off('click').on('click', function () {
+    $(".card-header-right .minimize-card").on('click', function () {
         var $this = $(this);
         var port = $($this.parents('.card'));
-        $(port).children('.card-block').slideToggle();
-        $(this).toggleClass("icon-minus icon-plus");
+        var card = $(port).children('.card-block').slideToggle();
+        $(this).toggleClass("icon-minus").fadeIn('slow');
+        $(this).toggleClass("icon-plus").fadeIn('slow');
     });
-
-    $(".card-header-right .full-card").off('click').on('click', function () {
+    $(".card-header-right .full-card").on('click', function () {
         var $this = $(this);
         var port = $($this.parents('.card'));
         port.toggleClass("full-card");
-        $(this).toggleClass("icon-maximize icon-minimize");
+        $(this).toggleClass("icon-maximize");
+        $(this).toggleClass("icon-minimize");
     });
 
-    // Mở chi tiết User/Search
-    $("#more-details").off('click').on('click', function () {
+    $("#more-details").on('click', function () {
         $(".more-details").slideToggle(500);
     });
-
-    $(".mobile-options").off('click').on('click', function () {
+    $(".mobile-options").on('click', function () {
         $(".navbar-container .nav-right").slideToggle('slow');
     });
-
-    // Scrollbar
+    // card js end
     $.mCustomScrollbar.defaults.axis = "yx";
+    $("#styleSelector .style-cont").slimScroll({
+        setTop: "10px",
+        height: "calc(100vh - 440px)",
+    });
     $(".main-menu").mCustomScrollbar({
         setTop: "10px",
         setHeight: "calc(100% - 80px)",
     });
+    /*chatbar js start*/
 
-    // Toggle Sidebar Mobile
-    $('#mobile-collapse').off('click').on('click', function () {
-        $('#mobile-collapse i').toggleClass('icon-toggle-right icon-toggle-left');
+    /*chat box scroll*/
+    var a = $(window).height() - 80;
+    $(".main-friend-list").slimScroll({
+        height: a,
+        allowPageScroll: false,
+        wheelStep: 5,
+        color: '#1b8bf9'
     });
 
-    // Initialize Tooltip
-    $('[data-toggle="tooltip"]').tooltip();
-
-    // Setting Theme Toggle (Phần quan quan trọng nhất cho SettingTheme.razor)
-    $(".selector-toggle > a").off('click').on('click', function () {
-        $('#styleSelector').toggleClass('open');
+    // search
+    $("#search-friends").on("keyup", function () {
+        var g = $(this).val().toLowerCase();
+        $(".userlist-box .media-body .chat-header").each(function () {
+            var s = $(this).text().toLowerCase();
+            $(this).closest('.userlist-box')[s.indexOf(g) !== -1 ? 'show' : 'hide']();
+        });
     });
-};
 
-// Hàm Toggle FullScreen (vẫn giữ nguyên như cũ)
-window.toggleFullScreen = function () {
-    if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+    // open chat box
+    $('.displayChatbox').on('click', function () {
+        var my_val = $('.pcoded').attr('vertical-placement');
+        if (my_val == 'right') {
+            var options = {
+                direction: 'left'
+            };
+        } else {
+            var options = {
+                direction: 'right'
+            };
+        }
+        $('.showChat').toggle('slide', options, 500);
+    });
+
+
+    //open friend chat
+    $('.userlist-box').on('click', function () {
+        var my_val = $('.pcoded').attr('vertical-placement');
+        if (my_val == 'right') {
+            var options = {
+                direction: 'left'
+            };
+        } else {
+            var options = {
+                direction: 'right'
+            };
+        }
+        $('.showChat_inner').toggle('slide', options, 500);
+    });
+    //back to main chatbar
+    $('.back_chatBox').on('click', function () {
+        var my_val = $('.pcoded').attr('vertical-placement');
+        if (my_val == 'right') {
+            var options = {
+                direction: 'left'
+            };
+        } else {
+            var options = {
+                direction: 'right'
+            };
+        }
+        $('.showChat_inner').toggle('slide', options, 500);
+        $('.showChat').css('display', 'block');
+    });
+    // /*chatbar js end*/
+    $(".search-btn").on('click', function () {
+        $(".main-search").addClass('open');
+        $('.main-search .form-control').animate({
+            'width': '200px',
+        });
+    });
+    $(".search-close").on('click', function () {
+        $('.main-search .form-control').animate({
+            'width': '0',
+        });
+        setTimeout(function () {
+            $(".main-search").removeClass('open');
+        }, 300);
+    });
+    $('#mobile-collapse i').addClass('icon-toggle-right');
+    $('#mobile-collapse').on('click', function () {
+        $('#mobile-collapse i').toggleClass('icon-toggle-right');
+        $('#mobile-collapse i').toggleClass('icon-toggle-left');
+    });
+});
+$(document).ready(function () {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+    $('.theme-loader').fadeOut('slow', function () {
+        //$(this).remove();
+    });
+});
+
+// toggle full screen
+function toggleFullScreen() {
+    var a = $(window).height() - 10;
+    if (!document.fullscreenElement && // alternative standard method
+        !document.mozFullScreenElement && !document.webkitFullscreenElement) { // current working methods
         if (document.documentElement.requestFullscreen) {
             document.documentElement.requestFullscreen();
         } else if (document.documentElement.mozRequestFullScreen) {
@@ -162,5 +168,187 @@ window.toggleFullScreen = function () {
             document.webkitCancelFullScreen();
         }
     }
-    $('.full-screen').toggleClass('icon-maximize icon-minimize');
-};
+    $('.full-screen').toggleClass('icon-maximize');
+    $('.full-screen').toggleClass('icon-minimize');
+}
+
+/* --------------------------------------------------------
+        Color picker - demo only
+        --------------------------------------------------------   */
+//$('#styleSelector').append('' +
+//    '<div class="selector-toggle">' +
+//    '<a href="javascript:void(0)"></a>' +
+//    '</div>' +
+//    '<ul>' +
+//    '<li>' +
+//    '<p class="selector-title main-title st-main-title"><b>Adminty </b>Customizer</p>' +
+//    '<span class="text-muted">Live customizer with tons of options</span>' +
+//    '</li>' +
+//    '<li>' +
+//    '<p class="selector-title">Main layouts</p>' +
+//    '</li>' +
+//    '<li>' +
+//    '<div class="theme-color">' +
+//    '<a href="#" class="navbar-theme" navbar-theme="themelight1"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="navbar-theme" navbar-theme="theme1"><span class="head"></span><span class="cont"></span></a>' +
+//    '</div>' +
+//    '</li>' +
+//    '</ul>' +
+//    '<div class="style-cont m-t-10">' +
+//    '<ul class="nav nav-tabs  tabs" role="tablist">' +
+//    '<li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#sel-layout" role="tab">Layouts</a></li>' +
+//    '<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#sel-sidebar-setting" role="tab">Sidebar Settings</a></li>' +
+//    '</ul>' +
+//    '<div class="tab-content tabs">' +
+//    '<div class="tab-pane active" id="sel-layout" role="tabpanel">' +
+//    '<ul>' +
+//    '<li class="theme-option">' +
+//    '<div class="checkbox-fade fade-in-primary">' +
+//    '<label>' +
+//    '<input type="checkbox" value="false" id="sidebar-position" name="sidebar-position" checked>' +
+//    '<span class="cr"><i class="cr-icon feather icon-check txt-success f-w-600"></i></span>' +
+//    '<span>Fixed Sidebar Position</span>' +
+//    '</label>' +
+//    '</div>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<div class="checkbox-fade fade-in-primary">' +
+//    '<label>' +
+//    '<input type="checkbox" value="false" id="header-position" name="header-position" checked>' +
+//    '<span class="cr"><i class="cr-icon feather icon-check txt-success f-w-600"></i></span>' +
+//    '<span>Fixed Header Position</span>' +
+//    '</label>' +
+//    '</div>' +
+//    '</li>' +
+//    '</ul>' +
+//    '</div>' +
+//    '<div class="tab-pane" id="sel-sidebar-setting" role="tabpanel">' +
+//    '<ul>' +
+//    '<li class="theme-option">' +
+//    '<p class="sub-title drp-title">Menu Type</p>' +
+
+//    '<div class="form-radio" id="menu-effect">' +
+//    '<div class="radio radio-inverse radio-inline" data-toggle="tooltip" title="simple icon">' +
+//    '<label>' +
+//    '<input type="radio" name="radio" value="st6" onclick="handlemenutype(this.value)" checked="true">' +
+//    '<i class="helper"></i><span class="micon st6"><i class="feather icon-command"></i></span>' +
+//    '</label>' +
+//    '</div>' +
+//    '<div class="radio  radio-primary radio-inline" data-toggle="tooltip" title="color icon">' +
+//    '<label>' +
+//    '<input type="radio" name="radio" value="st5" onclick="handlemenutype(this.value)">' +
+//    '<i class="helper"></i><span class="micon st5"><i class="feather icon-command"></i></span>' +
+//    '</label>' +
+//    '</div>' +
+//    '</div>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<p class="sub-title drp-title">SideBar Effect</p>' +
+//    '<select id="vertical-menu-effect" class="form-control minimal">' +
+//    '<option name="vertical-menu-effect" value="shrink">shrink</option>' +
+//    '<option name="vertical-menu-effect" value="overlay">overlay</option>' +
+//    '<option name="vertical-menu-effect" value="push">Push</option>' +
+//    '</select>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<p class="sub-title drp-title">Hide/Show Border</p>' +
+//    '<select id="vertical-border-style" class="form-control minimal">' +
+//    '<option name="vertical-border-style" value="solid">Style 1</option>' +
+//    '<option name="vertical-border-style" value="dotted">Style 2</option>' +
+//    '<option name="vertical-border-style" value="dashed">Style 3</option>' +
+//    '<option name="vertical-border-style" value="none">No Border</option>' +
+//    '</select>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<p class="sub-title drp-title">Drop-Down Icon</p>' +
+//    '<select id="vertical-dropdown-icon" class="form-control minimal">' +
+//    '<option name="vertical-dropdown-icon" value="style1">Style 1</option>' +
+//    '<option name="vertical-dropdown-icon" value="style2">style 2</option>' +
+//    '<option name="vertical-dropdown-icon" value="style3">style 3</option>' +
+//    '</select>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<p class="sub-title drp-title">Sub Menu Drop-down Icon</p>' +
+//    '<select id="vertical-subitem-icon" class="form-control minimal">' +
+//    '<option name="vertical-subitem-icon" value="style1">Style 1</option>' +
+//    '<option name="vertical-subitem-icon" value="style2">style 2</option>' +
+//    '<option name="vertical-subitem-icon" value="style3">style 3</option>' +
+//    '<option name="vertical-subitem-icon" value="style4">style 4</option>' +
+//    '<option name="vertical-subitem-icon" value="style5">style 5</option>' +
+//    '<option name="vertical-subitem-icon" value="style6">style 6</option>' +
+//    '</select>' +
+//    '</li>' +
+//    '</ul>' +
+//    '</div>' +
+//    '<ul>' +
+//    '<li>' +
+//    '<p class="selector-title">Header Brand color</p>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<div class="theme-color">' +
+//    '<a href="#" class="logo-theme" logo-theme="theme1"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="logo-theme" logo-theme="theme2"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="logo-theme" logo-theme="theme3"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="logo-theme" logo-theme="theme4"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="logo-theme" logo-theme="theme5"><span class="head"></span><span class="cont"></span></a>' +
+//    '</div>' +
+//    '</li>' +
+//    '<li>' +
+//    '<p class="selector-title">Header color</p>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<div class="theme-color">' +
+//    '<a href="#" class="header-theme" header-theme="theme1"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="header-theme" header-theme="theme2"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="header-theme" header-theme="theme3"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="header-theme" header-theme="theme4"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="header-theme" header-theme="theme5"><span class="head"></span><span class="cont"></span></a>' +
+//    '<a href="#" class="header-theme" header-theme="theme6"><span class="head"></span><span class="cont"></span></a>' +
+//    '</div>' +
+//    '</li>' +
+//    '<li>' +
+//    '<p class="selector-title">Active link color</p>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<div class="theme-color">' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme1">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme2">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme3">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme4">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme5">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme6">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme7">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme8">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme9">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme10">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme11">&nbsp;</a>' +
+//    '<a href="#" class="active-item-theme small" active-item-theme="theme12">&nbsp;</a>' +
+//    '</div>' +
+//    '</li>' +
+//    '<li>' +
+//    '<p class="selector-title">Menu Caption Color</p>' +
+//    '</li>' +
+//    '<li class="theme-option">' +
+//    '<div class="theme-color">' +
+//    '<a href="#" class="leftheader-theme small" lheader-theme="theme1">&nbsp;</a>' +
+//    '<a href="#" class="leftheader-theme small" lheader-theme="theme2">&nbsp;</a>' +
+//    '<a href="#" class="leftheader-theme small" lheader-theme="theme3">&nbsp;</a>' +
+//    '<a href="#" class="leftheader-theme small" lheader-theme="theme4">&nbsp;</a>' +
+//    '<a href="#" class="leftheader-theme small" lheader-theme="theme5">&nbsp;</a>' +
+//    '<a href="#" class="leftheader-theme small" lheader-theme="theme6">&nbsp;</a>' +
+//    '</div>' +
+//    '</li>' +
+//    '</ul>' +
+//    '</div>' +
+//    '</div>' +
+//    '<ul>' +
+//    '<li>' +
+//    '<a href="http://html.codedthemes.com/Adminty/doc" target="_blank" class="btn btn-primary btn-block m-r-15 m-t-5 m-b-10">Online Documentation</a>' +
+//    '</li>' +
+//    '<li class="text-center">' +
+//    '<span class="text-center f-18 m-t-15 m-b-15 d-block">Thank you for sharing !</span>' +
+//    '<a href="#!" target="_blank" class="btn btn-facebook soc-icon m-b-20"><i class="feather icon-facebook"></i></a>' +
+//    '<a href="#!" target="_blank" class="btn btn-twitter soc-icon m-l-20 m-b-20"><i class="feather icon-twitter"></i></a>' +
+//    '</li>' +
+//    '</ul>' +
+//    '');
--- QLVN-Blazor/wwwroot/js/themeBridge.js
+++ QLVN-Blazor/wwwroot/js/themeBridge.js
@@ -1,29 +1,117 @@
-﻿window.themeBridge = {
-    applySettings: function (settings) {
-        const pcoded = document.getElementById('pcoded');
-        if (!pcoded) return;
+﻿//window.themeBridge = {
+//    applySettings: function (settings) {
+//        const pcoded = document.getElementById('pcoded');
+//        if (!pcoded) return;
 
-        // 1. Layout Options
-        pcoded.setAttribute('pcoded-header-position', settings.isFixedHeader ? 'fixed' : 'relative');
-        pcoded.setAttribute('pcoded-navbar-position', settings.isFixedSidebar ? 'fixed' : 'relative');
+//        // 1. Layout Options
+//        pcoded.setAttribute('pcoded-header-position', settings.isFixedHeader ? 'fixed' : 'relative');
+//        pcoded.setAttribute('pcoded-navbar-position', settings.isFixedSidebar ? 'fixed' : 'relative');
 
-        // 2. Sidebar Settings
-        pcoded.setAttribute('v-theme', settings.mainLayout);
-        pcoded.setAttribute('vertical-menu-effect', settings.sidebarEffect);
-        pcoded.setAttribute('vertical-border-style', settings.borderStyle);
-        pcoded.setAttribute('vertical-dropdown-icon', settings.dropdownIcon);
-        pcoded.setAttribute('vertical-subitem-icon', settings.subItemIcon);
+//        // 2. Sidebar Settings
+//        pcoded.setAttribute('v-theme', settings.mainLayout);
+//        pcoded.setAttribute('vertical-menu-effect', settings.sidebarEffect);
+//        pcoded.setAttribute('vertical-border-style', settings.borderStyle);
+//        pcoded.setAttribute('vertical-dropdown-icon', settings.dropdownIcon);
+//        pcoded.setAttribute('vertical-subitem-icon', settings.subItemIcon);
 
-        // Menu Type (st5/st6)
-        const navMenu = pcoded.querySelector('.pcoded-navbar');
-        if (navMenu) {
-            navMenu.setAttribute('pcoded-menutype', settings.menuType);
+//        // Menu Type (st5/st6)
+//        const navMenu = pcoded.querySelector('.pcoded-navbar');
+//        if (navMenu) {
+//            navMenu.setAttribute('pcoded-menutype', settings.menuType);
+//        }
+
+//        // 3. Colors
+//        pcoded.setAttribute('logo-theme', settings.headerBrandColor);
+//        pcoded.setAttribute('header-theme', settings.headerColor);
+//        pcoded.setAttribute('active-item-theme', settings.activeLinkColor);
+//        pcoded.setAttribute('lheader-theme', settings.menuCaptionColor);
+//    }
+//};
+
+
+// Đảm bảo đối tượng được khởi tạo trên window
+window.adminThemeBridge = {
+    // 1. Xử lý tất cả các dropdown: Border, Icon, Effect
+    setSelectOption: function (elementId, value) {
+        const $pcoded = $('#pcoded');
+        if ($pcoded.length === 0) return;
+
+        // Cập nhật giá trị cho thẻ select (nếu có)
+        const $el = $(`#${elementId}`);
+        if ($el.length) {
+            $el.val(value).trigger('change');
         }
 
-        // 3. Colors
-        pcoded.setAttribute('logo-theme', settings.headerBrandColor);
-        pcoded.setAttribute('header-theme', settings.headerColor);
-        pcoded.setAttribute('active-item-theme', settings.activeLinkColor);
-        pcoded.setAttribute('lheader-theme', settings.menuCaptionColor);
+        // Gán trực tiếp Attribute để CSS Adminty nhận diện
+        switch (elementId) {
+            case 'vertical-menu-effect':
+                $pcoded.attr('vertical-effect', value);
+                break;
+            case 'vertical-border-style':
+                $pcoded.attr('vborder-style', value);
+                break;
+            case 'vertical-dropdown-icon':
+                $pcoded.attr('dropdown-icon', value);
+                break;
+            case 'vertical-subitem-icon':
+                $pcoded.attr('subitem-icon', value);
+                break;
+        }
+        this.refreshLayout();
+    },
+
+    // 2. Sửa lỗi UI Fixed Header
+    setHeaderPosition: function (isFixed) {
+        const $pcoded = $('#pcoded');
+        const pos = isFixed ? "fixed" : "relative";
+
+        $pcoded.attr("headerpos", pos);
+        $('.pcoded-header').attr("pcoded-header-position", pos);
+
+        if (!isFixed) {
+            // Khi header không cố định, navbar thường chuyển sang absolute
+            $('.pcoded-navbar').attr("pcoded-navbar-position", "absolute");
+            $pcoded.attr("navbarpos", "absolute");
+        } else {
+            const isSidebarFixed = $('#sidebar-position').is(':checked');
+            const sPos = isSidebarFixed ? "fixed" : "absolute";
+            $('.pcoded-navbar').attr("pcoded-navbar-position", sPos);
+            $pcoded.attr("navbarpos", sPos);
+        }
+        this.refreshLayout();
+    },
+
+    setSidebarPosition: function (isFixed) {
+        const val = isFixed ? "fixed" : "absolute";
+        $('.pcoded-navbar').attr("pcoded-navbar-position", val);
+        $('#pcoded').attr("navbarpos", val);
+        this.refreshLayout();
+    },
+
+    refreshLayout: function () {
+        setTimeout(() => {
+            window.dispatchEvent(new Event('resize'));
+        }, 150);
+    },
+
+    setTheme: function (type, value) {
+        if (type === "navbar") $(`.navbar-theme[navbar-theme="${value}"]`).trigger('click');
+        else if (type === "logo") $(`.logo-theme[logo-theme="${value}"]`).trigger('click');
+        else if (type === "header") $(`.header-theme[header-theme="${value}"]`).trigger('click');
+        else if (type === "active-item") $(`.active-item-theme[active-item-theme="${value}"]`).trigger('click');
+        else if (type === "caption") $(`.leftheader-theme[lheader-theme="${value}"]`).trigger('click');
+        this.refreshLayout();
     }
+};
+
+
+// Đảm bảo các hàm khởi tạo layout của template được gọi lại nếu cần
+window.initAdminFunctions = function () {
+    if (typeof $ !== 'undefined' && typeof $.mCustomScrollbar !== 'undefined') {
+        $(".main-menu").mCustomScrollbar({
+            setTop: "10px",
+            setHeight: "calc(100% - 80px)",
+        });
+    }
+    return true;
 };
